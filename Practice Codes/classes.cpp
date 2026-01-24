d71c2446be1f4cf5854d3d8248690606d4aaabf5
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-24 09:34:25 +0530

    Add files via upload

--- Practice Codes/classes.cpp
+++ Practice Codes/classes.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
 using namespace std;
-class teacher{
-    private:
-        double salary;
+class student{
     public:
-    //properties / attribiute
+        int age;
         string name;
-        string dept;
-        string subject;
-    //method / member funtion 
-        void changeDept(string newDept){
-            dept   = newDept;
-        }
-        void salary_teacher(double sal){
-            salary = sal;
-        }
-        double get_salary(){
-            return salary;
-        }
+
+    student(string name, int age){
+        this -> name = name;
+        this -> age = age;
+    }
+    void display(){
+        cout << "Name: " << name
+             <<"\nAge: "  << age;
+    }
 };
 int main() {
-    teacher t1;
-    t1.name = "XYZ";
-    t1.dept = "CSE";
-    t1.subject = "CPP";
-    t1.salary_teacher(125000);
-    cout << t1.name << " " << endl;
-    cout << t1.dept << " " << endl;
-    cout << t1.subject << " " << endl;
-    cout << t1.get_salary() << " " << endl;
+    student s("Rajat",10);
+    s.display();
+
     return 0;
 }
